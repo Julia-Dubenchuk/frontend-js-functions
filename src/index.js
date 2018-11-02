c82276c0d55d9a48631d4be59c1b12047e15e46c
Yulia Dubenchuk <dubenchuk6@gmail.com>
2018-11-02 23:08:02 +0200

    change function max and sum

--- src/index.js
+++ src/index.js
@@ -1,70 +1,34 @@
-function numberArr (arg) {
-	let arrayArg = [];
-
-	for (let i = 0; i < arg.length; i++) {
-		arrayArg[i] = arg[i];
-	}
-
-	let arr = arrayArg.filter(item => (typeof item === 'number' ? item : 0));
-
-	return arr;
+function isNumber (item) {
+	return typeof item === 'number';
 }
 
-function min () {
-	let minValue;
-
-	if (arguments.length && arguments[0].length) {
-		let array = numberArr(arguments[0]);
-
-		if (array.length) {
-			minValue = array.reduce((minItem, current) => (minItem < current ? minItem : current));
-		}
-
-	}
-
-	return minValue;
+function getLess (minNumber, currentNumber) {
+	return minNumber < currentNumber ? minNumber : currentNumber;
 }
 
-function max () {
-	let maxValue;
-
-	if (arguments.length && arguments[0].length) {
-		let arr = numberArr(arguments[0]);
-
-		if (arr.length) {
-			maxValue = arr.reduce((maxItem, current) => (maxItem > current ? maxItem : current));
-		}
-
-	}
-
-	return maxValue;
+function getGreater (maxNumber, currentNumber) {
+	return maxNumber > currentNumber ? maxNumber : currentNumber;
 }
 
-function sum () {
-	let sumValue;
+function getSum (number, currentNumber) {
+	return number + currentNumber;
+}
 
-	if (arguments.length) {
-		let arr = numberArr(arguments);
-
-		if (arr.length) {
-			sumValue = arr.reduce((sumItem, current) => sumItem + current);
-		}
-
-	}
-
-	return sumValue;
+function min (numbersArray = []) {
+	return numbersArray.filter(isNumber)
+		.reduce(getLess, undefined);
 
 }
 
-// export { min, max, sum };
+function max (numbersArray = []) {
+	return numbersArray.filter(isNumber)
+		.reduce(getGreater, undefined);
+}
 
-console.log(min([7, 8, 9, 5, 1]));
-console.log(min());
-console.log(min(['j', 'k', 's', 'd']));
-console.log(min([-5, 3, 's', 'd']));
-console.log(min([5.5, 0.3, null, true]));
-console.log(max([7, 8, 9, 5, 1]));
-console.log(max());
-console.log(max([2, 2, 2, 2, 2]));
-console.log(sum(7, 8, 9, 5, 1));
-console.log(sum());
+function sum (...numbersArray) {
+	return numbersArray.filter(isNumber)
+		.reduce(getSum, 0);
+
+}
+
+export { min, max, sum };
